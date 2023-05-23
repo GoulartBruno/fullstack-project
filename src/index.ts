66bab9e212c3dd844e8da133e4aeefa8905c47c2
GoulartBruno <brunogoulartdev@gmail.com>
2023-05-23 16:31:30 +0200

    index chances, add routers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { postRouter } from "./router/postRouter";
+import { userRouter } from "./router/userRouter";
 
 dotenv.config();
 
@@ -11,4 +13,12 @@ app.use(express.json());
 
 app.listen(Number(process.env.PORT) || 3003, () => {
   console.log(`Servidor rodando na porta ${Number(process.env.PORT) || 3003}`);
+});
+
+app.use("/users", userRouter);
+
+app.use("/playlists", postRouter);
+
+app.get("/ping", (req, res) => {
+  res.send("Pong!");
 });
